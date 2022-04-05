475ba8f9ab13c51628f1903eef53e25f37d2a617
nivyapeter <nivyakpeter@gmail.com>
2022-04-05 14:47:09 +0530

    feat: links

--- react-router-demo/src/App.js
+++ react-router-demo/src/App.js
@@ -1,12 +1,17 @@
 import {Routes, Route} from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
+import Navbar from './Navbar';
+
 function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='about' element={<About />}></Route>
     </Routes>
+    </>
   );
 }
 
