89bf66fdc76e8bccb2b248174e0d49742511d47f
nivyapeter <nivyakpeter@gmail.com>
2022-04-12 21:23:29 +0530

    student list

---
+++ student-attendance-system/src/components/Columns.js
@@ -0,0 +1,14 @@
+export const Columns = [
+    {
+        header: 'Id',
+        accessor:'id'
+    },
+    {
+        header: 'Name',
+        accessor:'name'
+    },
+    {
+        header: 'Email',
+        accessor:'email'
+    },
+]
