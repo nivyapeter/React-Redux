e088a7b8c49435bff25dc5d4a5b3f28b6be289f3
nivyapeter <nivyakpeter@gmail.com>
2022-04-17 00:01:12 +0530

    material table component

--- codeevolution-redux-app/src/App.js
+++ codeevolution-redux-app/src/App.js
@@ -16,14 +16,45 @@
 
 // export default App;
 
-import React from "react";
- function App() {
-   return (
-     <div className="App">
-       <h1 className=" items-center">React-App</h1>
-       <h2 className="items-center">hai</h2>
+import React, { useState } from 'react';
+// import MaterialTable from 'material-table';
+// import {Routes, Route} from 'react-router-dom';
+// import './App.css';
+// import StudentMainList from '../src/student-management/StudentMainList';
+// import Login from '../src/student-management/Login';
 
-     </div>
-   )
- }
- export default App;
+
+function App() {
+  // const [token, setToken] = useState();
+
+  // if(!token) {
+  //   return <Login setToken={setToken} />
+  // }
+  const data = [
+    { name: "Mohammad", surname: "Faisal", birthYear: 1995 },
+    { name: "Nayeem Raihan ", surname: "Shuvo", birthYear: 1994 },
+  ];
+  
+  const columns = [
+    { title: "Name", field: "name" },
+    { title: "Surname", field: "surname" },
+    { title: "Birth Year", field: "birthYear", type: "numeric" },
+  ];
+  return (
+    <div className="wrapper">
+      <h1>Hai</h1>
+      <div>
+      {/* <MaterialTable title="Basic Table" columns={columns} data={data} />; */}
+      {/* <h1>Application</h1>
+      <Routes>
+          <Route path="/StudentMainList">
+            <StudentMainList />
+          </Route>
+          </Routes> */}
+          {/* <StudentMainList /> */}
+          </div>
+    </div>
+  );
+}
+
+export default App;
