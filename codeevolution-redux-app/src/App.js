2529c8137781d0fdc224c88b22a410898eed8046
nivyapeter <nivyakpeter@gmail.com>
2022-04-17 22:53:52 +0530

    feat: react table

--- codeevolution-redux-app/src/App.js
+++ codeevolution-redux-app/src/App.js
@@ -1,3 +1,44 @@
+import React, { useState} from "react"
+import data from "../src/student-management/data"
+import AllStudentList from "./student-management/AllStudentList"
+function App() {
+  
+  const [rowdata, setRowData] = useState(data)
+  const columns = [
+    {
+      Header: "id",
+      accessor: "id",
+    },
+    {
+      Header: "admission_number",
+      accessor: "admission_number",
+    },
+    {
+      Header: "first_name",
+      accessor: "first_name",
+    },
+    {
+      Header: "last_name",
+      accessor: "last_name",
+    },
+    {
+      Header: "fee_status",
+      accessor: "fee_status",
+    },
+  ]
+  
+  return (
+    <div className="bg-purple-300 h-screen">
+    <div className="container m-auto w-11/12">
+      <h2 className="text-center font-extrabold ">All Students List</h2>
+      <div className="flex justify-center mt-8">
+        <AllStudentList columns={columns} data={rowdata} />
+      </div>
+    </div>
+    </div>
+  )
+}
+export default App
 // import './App.css';
 // import { Provider } from 'react-redux'
 // import store from './redux/store';
@@ -16,7 +57,7 @@
 
 // export default App;
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 // import MaterialTable from 'material-table';
 // import {Routes, Route} from 'react-router-dom';
 // import './App.css';
@@ -24,27 +65,27 @@ import React, { useState } from 'react';
 // import Login from '../src/student-management/Login';
 
 
-function App() {
+// function App() {
   // const [token, setToken] = useState();
 
   // if(!token) {
   //   return <Login setToken={setToken} />
   // }
-  const data = [
-    { name: "Mohammad", surname: "Faisal", birthYear: 1995 },
-    { name: "Nayeem Raihan ", surname: "Shuvo", birthYear: 1994 },
-  ];
+  // const data = [
+  //   { name: "Mohammad", surname: "Faisal", birthYear: 1995 },
+  //   { name: "Nayeem Raihan ", surname: "Shuvo", birthYear: 1994 },
+  // ];
   
-  const columns = [
-    { title: "Name", field: "name" },
-    { title: "Surname", field: "surname" },
-    { title: "Birth Year", field: "birthYear", type: "numeric" },
-  ];
-  return (
-    <div className="wrapper">
-      <h1>Hai</h1>
-      <div>
-      {/* <MaterialTable title="Basic Table" columns={columns} data={data} />; */}
+  // const columns = [
+  //   { title: "Name", field: "name" },
+  //   { title: "Surname", field: "surname" },
+  //   { title: "Birth Year", field: "birthYear", type: "numeric" },
+  // ];
+  // return (
+  //   <div className="wrapper">
+  //     <h1>Hai</h1>
+  //     <div>
+  //     <MaterialTable className="bg-blue-600" title="Basic Table" columns={columns} data={data} />;
       {/* <h1>Application</h1>
       <Routes>
           <Route path="/StudentMainList">
@@ -52,9 +93,13 @@ function App() {
           </Route>
           </Routes> */}
           {/* <StudentMainList /> */}
-          </div>
-    </div>
-  );
-}
+//           </div>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+
+
+
