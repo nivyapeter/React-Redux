0023ce8c4bd874f7c6f97266cba2a4c855a32a20
nivyapeter <nivyakpeter@gmail.com>
2022-04-18 23:31:23 +0530

    feat:editable button

--- codeevolution-redux-app/src/App.js
+++ codeevolution-redux-app/src/App.js
@@ -1,9 +1,122 @@
 import React, { useState} from "react"
 import data from "../src/student-management/data"
 import AllStudentList from "./student-management/AllStudentList"
+// import Login from "../src/student-management/Login"
+import EditableRow from "./student-management/EditableRow"
+import ReadOnlyRow  from "./student-management/ReadOnlyRow"
 function App() {
+
   
   const [rowdata, setRowData] = useState(data)
+
+
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    admission_number: "",
+    first_namess: "",
+    last_name: "",
+    fee_status: "",
+  });
+
+  const [editFormData, setEditFormData] = useState({
+    admission_number: "",
+    first_namess: "",
+    last_name: "",
+    fee_status: "",
+  });
+
+  const [editContactId, setEditContactId] = useState(null);
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  };
+
+  const handleEditFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...editFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setEditFormData(newFormData);
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+      id: addFormData.AddDta,
+      admission_number: addFormData.fullName,
+      first_namess: addFormData.address,
+      last_name: addFormData.phoneNumber,
+      fee_status: addFormData.email,
+    };
+
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  };
+
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault();
+
+    const editedContact = {
+      id: editContactId,
+      admission_number: editFormData.fullName,
+      first_namess: editFormData.address,
+      last_name: editFormData.phoneNumber,
+      fee_status: editFormData.email,
+    };
+
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === editContactId);
+
+    newContacts[index] = editedContact;
+
+    setContacts(newContacts);
+    setEditContactId(null);
+  };
+
+  const handleEditClick = (event, contact) => {
+    event.preventDefault();
+    setEditContactId(contact.id);
+
+    const formValues = {
+      admission_number: contact.fullName,
+      first_namess: contact.address,
+      last_name: contact.phoneNumber,
+      fee_status: contact.email,
+    };
+
+    setEditFormData(formValues);
+  };
+
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  };
+
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    newContacts.splice(index, 1);
+
+    setContacts(newContacts);
+  };
+
+
+
   const columns = [
     {
       Header: "id",
@@ -25,6 +138,11 @@ function App() {
       Header: "fee_status",
       accessor: "fee_status",
     },
+    {
+      Header: "actions",
+      accessor: "actions",
+    },
+    
   ]
   
   return (
@@ -33,6 +151,7 @@ function App() {
       <h2 className="text-center font-extrabold ">All Students List</h2>
       <div className="flex justify-center mt-8">
         <AllStudentList columns={columns} data={rowdata} />
+        {/* <Login /> */}
       </div>
     </div>
     </div>
